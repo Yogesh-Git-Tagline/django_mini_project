e1842ded9d4cec71fb38e5ec6c977f9fdf9106d5
YogeshC <yogeshc.tagline.com>
2023-11-28 16:38:01 +0530

    small improvement

--- school_app/views.py
+++ school_app/views.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 
+
 class HomePage(TemplateView):  # this class just for render the home page template
     template_name = 'home.html'
 
@@ -16,32 +17,35 @@ class StudentList(ListView):  # this class displays list of all students on temp
     queryset = Student.objects.all()
     template_name = 'student_list.html'
     context_object_name = 'students'
-    paginate_by=3
+    paginate_by = 3
 
 
-class AddStudent(SuccessMessageMixin,CreateView):  # this class displays form for create new student
+# this class displays form for create new student
+class AddStudent(SuccessMessageMixin, CreateView):
     model = Student
     form_class = AddStudentForm
     template_name = 'add_student.html'
     context_object_name = 'form'
     success_url = '/StudentList/'
-    success_message='Student Added Successfully!!!'
+    success_message = 'Student Added Successfully!!!'
 
 
-class UpdateStudent(SuccessMessageMixin,UpdateView):  # this class displays form for update existing student
+# this class displays form for update existing student
+class UpdateStudent(SuccessMessageMixin, UpdateView):
     model = Student
     form_class = AddStudentForm
     template_name = 'update_student.html'
     context_object_name = 'form'
     success_url = '/StudentList/'
-    success_message='Updated successfully!!!'
+    success_message = 'Updated successfully!!!'
 
 
 # this function takes id of specific student and delete that student
 def delete_student(request, pk):
     student = Student.objects.get(id=pk)
     student.delete()
-    messages.add_message(request,messages.SUCCESS,'Student Deleted Successfully...')
+    messages.add_message(request, messages.SUCCESS,
+                         'Student Deleted Successfully...')
     return HttpResponseRedirect('/StudentList/')
 
 
